701bb47e1862eb28235ed233b291ca9d7d022c1f
raj-umang <jackerumang@gmail.com>
2024-07-01 11:56:27 +0530

    csv & pdf file

--- proj11/P11/views.py
+++ proj11/P11/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from reportlab.pdfgen import canvas
-from  P11.models import Course, Student, ProjectReg
+from reportlab.pdfgen import canvas
+from  P11.models import Course
 import csv
 
 # Create your views here.
 def construct_csv_from_model(request):
-    courses = Course.objects.all()
-    response = HttpResponse(content_type = "text/csv")
-    response['Content-Disposition'] = 'attachment; filename = "course_data.csv" '
-    writer = csv.writer(response)
-    writer.writerow(['Course Name', 'Course Code', 'Course Credits'])
-    for course in courses:
-        writer.writerow([course.course_name, course.course_code, course.course_credits])
+    if 'download' in request.GET:
+        courses = Course.objects.all()
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="course_data.csv"'
+        
+        writer = csv.writer(response)
+        writer.writerow(['Course Name', 'Course Code', 'Course Credits'])
+        
+        for course in courses:
+            writer.writerow([course.course_name, course.course_code, course.course_credits])
+        
+        return response
+    
+    return render(request, 'csv.html')
+
+def construct_pdf_from_model(request): 
+    courses=Course.objects.all()
+    response=HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = ' attachment; filename="courses_data.pdf" '
+    c=canvas.Canvas(response)
+    c.drawString(70,720,"Course Name")
+    c.drawString(170,720,"Course Code")
+    c.drawString(270,720,"Credits") 
+    y=660
+    for course in courses: 
+        c.drawString(70,y,course.course_name)
+        c.drawString(170,y,course.course_code)
+        c.drawString(270,y,str(course.course_credits)) 
+        y=y-60
+    c.showPage() 
+    c.save()
     return response
--- proj11/proj11/urls.py
+++ proj11/proj11/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from P11.views import construct_csv_from_model
+from P11.views import construct_csv_from_model, construct_pdf_from_model
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('construct_csv_from_model/', construct_csv_from_model, name='construct_csv_from_model')
+    path('construct_csv_from_model/', construct_csv_from_model, name='construct_csv_from_model'),
+    path('pdf/', construct_pdf_from_model, name='construct_pdf_from_model'),
 ]
